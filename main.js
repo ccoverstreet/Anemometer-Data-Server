95f199a76fcdfec7304800e3639a8694ea643a93
ccoverstreet <coverst2@vols.utk.edu>
2020-12-07 20:57:39 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+const port = 8080
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(function (req, res, next) {
+	console.log(req.body)
+	next()
+})
+
+app.post("/", (req, res) => {
+	res.send("good")
+})
+
+app.get("/tester", (req, res) => {
+	console.log(req)
+})
+
+app.post("/tester", (req, res) => {
+	console.log(req)
+})
+
+app.listen(port, () => {
+	console.log("Started Server.")
+})
