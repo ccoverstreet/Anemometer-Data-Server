a32921dd8251f7500d6eb65f54c057ea8c888b09
ccoverstreet <coverst2@vols.utk.edu>
2021-01-14 23:48:36 +0000

    Added data dir creation on startup

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const fs = require("fs");
 
 const app = express()
 const port = 8080
 
+const dataTypes = require("./src/data.js");
+
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(function (req, res, next) {
-	console.log(req.body)
-	next()
+	console.log(req.body);
+	next();
 })
 
 app.post("/", (req, res) => {
-	res.send("good")
+	res.send("good");
 })
 
 app.get("/dump", (req, res) => {
-	console.log(req)
+	console.log(req);
 })
 
 app.post("/dump", (req, res) => {
-	console.log(req)
+	console.log(req);
 })
 
 app.listen(port, () => {
-	console.log("Started Server.")
+	console.log("Started Anemometer Data Server.");
+	console.log("Checking if \"data\" dir exists.");
+	if (!fs.existsSync("./data")) {
+		console.log("\"data\" dir not found. Creating dir.");
+		fs.mkdirSync("./data", 0755);
+	}
+	console.log("Ready for data.");
 })
